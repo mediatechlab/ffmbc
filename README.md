a0d2eb8fa6cef4bf6de3d814619c038b53df7b2a
Alvaro Lopez Antelo <alvinho@tvglobo.com.br>
2020-10-26 12:56:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+FFmbc usage:
+
+In order to generate a **Sony MXF XDCAM HD** file
+
+ffmbc -i input.MOV -tff -target xdcamhd422 -an output.MXF -acodec pcm_s24le -ar 48000 -newaudio -acodec pcm_s24le -ar 48000 -newaudio -map_audio_channel 0:1:0:0:1:0 -map_audio_channel 0:1:0:0:2:0
+
+To generate a **Sony MXF IMX50 SD** file
+
+ffmbc -i input.MOV -tff -target imx50 -an -y output.MXF -acodec pcm_s24le -ar 48000 -newaudio -acodec pcm_s24le -ar 48000 -map_audio_channel 0:1:0:0:1:0
---
+++ compile_ffmbc.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y build-essential yasm \
+libgpac-dev libgsm1-dev libspeex-dev \
+libvorbis-dev libdc1394-22-dev libsdl1.2-dev \
+zlib1g-dev texi2html libfaac-dev libmp3lame-dev \
+libtheora-dev libopencore-amrnb-dev \
+libopencore-amrwb-dev frei0r-plugins-dev \
+libopencv-dev libvpx-dev libgavl1 libx264-dev
+
+git clone https://github.com/bcoudurier/FFmbc
+cd FFmbc
+./configure --enable-gpl --enable-nonfree --enable-shared --enable-postproc --enable-runtime-cpudetect --enable-frei0r --enable-libdc1394 --enable-libfaac --enable-libgsm --enable-libmp3lame --enable-libspeex --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-pthreads --enable-zlib --disable-doc
+make -j$(nproc)
+sudo make install 
+sudo ldconfig
